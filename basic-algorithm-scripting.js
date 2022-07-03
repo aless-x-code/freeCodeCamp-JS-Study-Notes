0c3c6b7f218d8384f8ddc3e2d0659d1af6658852
Alesandro <aless.cam.h@gmail.com>
2022-07-03 13:59:32 -0400

    updated 7/2/22

--- basic-algorithm-scripting.js
+++ basic-algorithm-scripting.js
@@ -124,8 +124,9 @@ console.log(booWho(false));
 //_____________________________________________________
 // Title Case
 
-function titleCase(str) { // 
-  str = str.toLowerCase().split(" "); 
+function titleCase(str) {
+  //
+  str = str.toLowerCase().split(" ");
   for (let i = 0; i < str.length; i++) {
     str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
   }
@@ -137,10 +138,79 @@ console.log(titleCase("I'm a little tea pot"));
 //_____________________________________________________
 // Copy one array into another, beggining at index n
 
-function frankenSplice(arr1, arr2, n) { 
+function frankenSplice(arr1, arr2, n) {
   let clone = [...arr2]; // cloning arr2 so it goes unmodified globally
-  clone.splice(n, 0, ...arr1) // splice(starting index, delete # elements, what to add) (... for it to copy as 1, 2, 3, rather than [1, 2, 3])
+  clone.splice(n, 0, ...arr1); // splice(starting index, delete # elements, what to add) (... for it to copy as 1, 2, 3, rather than [1, 2, 3])
   return clone;
 }
 
 console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
+
+//_____________________________________________________
+// Falsy Bouncer
+// check for boolean value, primitive
+console.log(Boolean(x));
+console.log(!!x);
+
+// function that bounces false items
+function bouncer(arr) {
+  let answerArray = []; // working array
+  for (let i = 0; i < arr.length; i++) {
+    // iterate through array elements
+    if (!!arr[i] == false) {
+      // if element false, delete
+      delete arr[i];
+    } else {
+      answerArray.push(arr[i]); // if true, push to working array
+    }
+  }
+  return answerArray;
+}
+console.log(bouncer([7, "ate", "", NaN, false, 9]));
+
+//_____________________________________________________
+// Where does an element belong chronologically, (index wise)
+
+function getIndexToIns(arr, num) {
+  // function that returns the index where a numbers belong chronologically (if [40, 60], 50 belongs at = [1])
+  arr.splice(0, 0, num); //arr.push(num); // adding num to the array
+  arr.sort(function (a, b) {
+    // BLUEPRINT: number sorting function
+    return a - b;
+  });
+  return arr.indexOf(num); // once sorted, return index of where element belonged
+}
+console.log(getIndexToIns([40, 60], 50));
+
+//_____________________________________________________
+// compare 2 strings in an array, return true if first words has all the characters of word 2
+
+
+function mutation(arr) {
+  let word0 = arr[0].split("");
+  let word1 = arr[1].split(""); // array into 2 words; word0 and word1
+
+  const word0Lower = word0.map(word0 => word0.toLowerCase());
+  const word1Lower = word1.map(word1 => word1.toLowerCase()); // lower casing word, for standarization
+
+
+  let matchingCharacters = []; // basket of matching characters
+  for (let i = 0; i < word0Lower.length; i++) { // iterating through word0
+    if (word1Lower.indexOf(word0Lower[i]) >= 0) { // if word0 characters are in word1 ....
+      matchingCharacters.push(word0Lower[i]); // push matching characters into basket
+    }
+  }
+  console.log(matchingCharacters); // matching characters
+
+  const containsAll = word1Lower.every(element => { // if all elements of word1 are in matchign character's basket, return true
+    return matchingCharacters.indexOf(element) !== -1;
+  });
+  return(containsAll); 
+
+}
+
+console.log(mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"])); 
+
+
+//_____________________________________________________
+// 
